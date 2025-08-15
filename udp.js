56ac4d896407a7a4f7c63dd888b6da3c7dce37c0
Hamdan-khan <Hamdankhan212@gmail.com>
2025-08-15 17:44:49 +0500

    Graceful closing + connection

--- udp.js
+++ udp.js
@@ -1,13 +1,34 @@
 const dgram = require("dgram");
-const server = dgram.createSocket("udp4");
+const readline = require("readline");
 
+const server = dgram.createSocket("udp4");
 server.connect(42069, "127.0.0.1");
 
-// "data" event is emitted on newLine (when enter key is registered)
-process.stdin.on("data", (buffer) => {
-  console.log(buffer);
-  server.send(buffer);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
+
+rl.setPrompt("> ");
+rl.prompt();
+
+rl.on("line", (line) => {
+  server.send(Buffer.from(line));
+  rl.prompt();
+});
+
+rl.on("SIGINT", () => {
+  console.log("\nClosing UDP sender...");
+  server.disconnect();
+  server.close();
+  process.exit(0);
+});
+
+// "data" event is emitted on newLine (when enter key is registered)
+// process.stdin.on("data", (buffer) => {
+//   console.log(buffer);
+//   server.send(buffer);
+// });
 
 // to listen for UDP Packets in powershell:
 // & "C:\Program Files (x86)\Nmap\ncat.exe" -u -l 42069
