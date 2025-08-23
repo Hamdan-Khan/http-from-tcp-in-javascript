cba73ce1a2a118339cec80e25dc6d3dc0f9dde75
Hamdan-khan <Hamdankhan212@gmail.com>
2025-08-23 21:14:13 +0500

    add request line parsing function

--- request/src/request.test.ts
+++ request/src/request.test.ts
@@ -1,8 +1,19 @@
+const { Readable } = require("stream");
+const { RequestFromReader } = require("./request");
+
 test("Good requeest line", () => {
-  const n = null;
-  expect(n).toBeNull();
-  expect(n).toBeDefined();
-  expect(n).not.toBeUndefined();
-  expect(n).not.toBeTruthy();
-  expect(n).toBeFalsy();
+  const r = RequestFromReader(
+    Readable.from(
+      "GET / HTTP/1.1\r\nHost: localhost:42069\r\nUser-Agent: curl/7.81.0\r\nAccept: */*\r\n\r\n",
+    ),
+  );
+
+  if (r) {
+    expect(r).toHaveProperty("method");
+    expect(r).toHaveProperty("requestTarget");
+    expect(r).toHaveProperty("httpVersion");
+    expect(r.method).toBe("GET");
+    expect(r.requestTarget).toBe("/");
+    expect(r.httpVersion).toBe("1.1");
+  }
 });
--- request/src/request.ts
+++ request/src/request.ts
@@ -10,8 +10,73 @@ interface RequestLine {
   method: string;
 }
 
-export function RequestFromReader(stream: Readable) {
+/**
+ * New line
+ */
+const CRLF = "\r\n" as const;
+
+/**
+ * ASCII code ranges for capital and lower case letters
+ */
+const ASCII_RANGE = {
+  capitalStart: 65,
+  capitalEnd: 90,
+  lowerStart: 97,
+  lowerEnd: 122,
+} as const;
+
+/**
+ * HTTP version
+ */
+const HTTP_VERSION = "HTTP/1.1" as const;
+
+function parseRequestLine(message: string): null | RequestLine {
+  const newLineIndex = message.search(CRLF);
+
+  // no newline found
+  if (newLineIndex === -1) {
+    return null;
+  }
+
+  // possible start-line
+  const candidate = message.substring(0, newLineIndex);
+  // request line parts (method, target, http-ver) separated by spaces
+  const parts = candidate.split(" ");
+
+  if (parts.length !== 3) {
+    return null;
+  }
+
+  const method = parts[0];
+  const requestTarget = parts[1];
+  const httpVersion = parts[2];
+
+  // to validate the request line parts
+  // method should be upper-case
+  for (let i = 0; i < method.length; i++) {
+    if (
+      method.charCodeAt(i) < ASCII_RANGE.capitalStart ||
+      method.charCodeAt(i) > ASCII_RANGE.capitalEnd
+    ) {
+      return null;
+    }
+  }
+  // http version should be 1.1
+  if (httpVersion !== HTTP_VERSION) {
+    return null;
+  }
+
+  return {
+    method,
+    requestTarget,
+    httpVersion,
+  };
+}
+
+export function RequestFromReader(stream: Readable): RequestLine | null {
+  let requestLine = null;
   stream.on("data", (chunk) => {
-    console.log(chunk);
+    requestLine = parseRequestLine(chunk);
   });
+  return requestLine;
 }
